72e707caf0bd38afb38d11b9d7a857a75dc8c31f
sumeshofficial <sumeshsujesh1233@gmail.com>
2025-09-24 20:38:33 +0530

    created env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "***REMOVED***",
-  authDomain: "sel-resell-application.firebaseapp.com",
-  projectId: "sel-resell-application",
-  storageBucket: "sel-resell-application.firebasestorage.app",
-  messagingSenderId: "945637230600",
-  appId: "1:945637230600:web:45d98e28115abbe2c669bd",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
